801ca70fdd735e7747b2f020453946c2aa46ab30
staubi82 <staubi1982@gmail.com>
2026-03-22 22:53:30 +0100

    feat: pass last7Days data to dashboard view

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -4,6 +4,7 @@ const dashboardService = require('../services/dashboardService');
 function showDashboard(req, res, next) {
   try {
     const stats = dashboardService.getStats();
+    stats.last7Days = dashboardService.getLast7DaysActivity();
     res.render('dashboard', { stats, username: req.session.username });
   } catch (err) { next(err); }
 }
